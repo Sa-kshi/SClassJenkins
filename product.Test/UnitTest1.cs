9127db508acaed37ab781534f5d3ab57c360c674
Sakshi Tomar <sakshitomar@Sakshis-MacBook-Pro.local>
2025-12-09 00:30:57 +0530

    First Commit

---
+++ product.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using product;
+using Xunit;
+
+namespace product.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void AddTwoNumbers_ReturnSum()
+    {
+        var result = Product.Add(2, 7);
+        Assert.Equal(5, result);
+    }
+}
---
+++ product/Product.cs
@@ -0,0 +1,8 @@
+using System;
+namespace product
+{
+    public class Product
+    {
+        public static int Add(int a, int b) => a+b;
+    }
+}
